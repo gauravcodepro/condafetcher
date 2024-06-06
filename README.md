1f1635aa366d64a2a8fce1aeead8017317b03c57
Gaurav <gaurav.sablok@uni-potsdam.de>
2024-06-06 16:15:04 +0200

    a pure implementation of regular expression for fetching all the related anaconda repository for devops. A much after than actual page loading.  just a short bit while coding for matlab metabolic models.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # condafetcher
+
+- a condafetcher that interacts directly with the anaconda package repository
+- to run 
+```
+sh condacheck.sh
+please provide the search term:seaborn
+
+```
+
+``` ``` conda install conda-forge::seaborn conda install conda-forge::seaborn-base conda install singleronbio::seaborn-base conda install conda-forge::seaborn-image conda 
+install christofsteel::seaborn-minimal conda install anaconda::seaborn conda install activisiongamescience::seaborn
+```
+
+Gaurav \ 
+Academic Staff Member \ 
+Bioinformatics \ 
+Institute for Biochemistry and Biology \ 
+University of Potsdam \ 
+Potsdam,Germany
---
+++ condafetcher.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+# Author Gaurav
+# Universitat Potsdam
+# Date 2024-6-6
+
+# A way to connect to the conda and search for the terms and get the conda generators.
+# a pure implementation of regular expression.
+read -r -p "please provide the search term:" term
+wget -F "https://anaconda.org/search?q=${term}" -O condasearch.txt 
+echo "all term fetched"
+cat condasearch.txt | grep ${term} | \
+         grep href | cut -f 2 -d "=" \
+              | cut -f 1 -d ">" | grep -w ${term} > saveintermediate.txt  
+echo "all intermediate generated"              
+for i in $(cat saveintermediate.txt); do echo \
+        "https@//anaconda.org$i"; done | \
+                     sed "s/\"//g" | sed "s/@/:/g" > downloadlinks.txt 
+echo "moving into intermediate"                     
+mkdir intermediate
+cp -r downloadlinks.txt ./intermediate
+cd intermediate
+cat downloadlinks.txt | while read line; \
+          do wget -F ${line}; done
+cat * | grep -w install | grep ${term} > newsave.txt
+cat newsave.txt | grep code | cut -f 2 -d ">" | cut -f 1 -d "<"
+cd ..
+rm -rf intermediate
+rm -rf saveintermediate.txt finaltext.txt downloadlinks.txt condasearch.txt
